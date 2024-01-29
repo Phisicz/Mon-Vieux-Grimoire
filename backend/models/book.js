4b8d750e894c0ec7930b36c2e92289aac5987669
Iuri <nicoguloso2@gmail.com>
2024-01-29 17:57:37 +0100

    fix: change schema and add missing attributes

--- backend/models/book.js
+++ backend/models/book.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const bookSchema = mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  year: { type: String, required: true },
-  category: {type: String, required: true},
-  review: {type: Number, required: true},
+  year: { type: Number, required: true },
+  genre: {type: String, required: true},
+  rating: {type: Number, required: true},
   userId: { type: String, required: true },
+  imageUrl: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Book', bookSchema);
