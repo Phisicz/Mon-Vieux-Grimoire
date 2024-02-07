fe7b79c8d2997f35dc45530cd64bda6cdcafcb67
Iuri <nicoguloso2@gmail.com>
2024-02-07 01:48:52 +0100

    feat: ratings schema including userId, grade and averageRating

--- backend/models/book.js
+++ backend/models/book.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-  userId: { type: String, required: true },
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  year: { type: Number, required: true },
-  genre: {type: String, required: true},
-  rating: {type: Number, required: true},
+  userId: { type: String, required: true }, //user unique mongodb
+  title: { type: String, required: true }, //titre du livre
+  author: { type: String, required: true }, //auteur du livre
+  imageUrl: { type: String, required: true }, //url de la couverture du livre
+  year: { type: Number, required: true }, //année de publication du livre
+  genre: {type: String, required: true}, //genre du livre
+  ratings: {
+    userId: { type: String, required: true }, //user unique mongodb qui a noté le livre
+    grade: { type: Number, required: true }, //note d'un livre
+    averageRating: { type: Number, required: true } //note moyenne du livre
+  }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
