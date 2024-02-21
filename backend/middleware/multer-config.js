a936d4e367b2d8a84558b03507cf54a980382d4e
Iuri <nicoguloso2@gmail.com>
2024-02-21 15:08:14 +0100

    feat: add image size limit on multer middleware

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -17,4 +17,4 @@ const storage = multer.diskStorage({
   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({storage: storage, limits: {fileSize: 2 * 1024 * 1024,},}).single("image");
