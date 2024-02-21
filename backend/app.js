9bb1f51fbff536d33c49b4496f86102a7dcd21bb
Iuri <nicoguloso2@gmail.com>
2024-02-21 18:01:35 +0100

    refactor: add dotenv and change mongoose url to use .env variable

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const Book = require ('./models/Book');
@@ -7,6 +9,8 @@ const userRoutes = require('./routes/user');
 const app = express();
 const path = require ('path');
 
+const databaseConfig = require("./configs/databaseConfig");
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -14,14 +18,10 @@ app.use((req, res, next) => {
     next();
 });
 
-mongoose.connect('mongodb+srv://adminOC:cYOj87wWszpWWsPe@databaseoc.6ynao2m.mongodb.net/books?retryWrites=true&w=majority')
-.then(() => {
-    console.log('Successfully connected to MongoDB Community Atlas!')
-})
-.catch((error) => {
-    console.log('Unable to connect to MongoDB Community Atlas!');
-    console.error(error);
-});
+mongoose
+    .connect(databaseConfig.dbUri)
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
 app.use(express.json());
 
