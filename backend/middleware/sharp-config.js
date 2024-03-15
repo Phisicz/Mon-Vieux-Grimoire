64a35488f4c23bcdcf92fb9c0d7e9fc3369b98a6
Iuri <nicoguloso2@gmail.com>
2024-03-15 23:32:42 +0100

    fix: add extra error steps

--- backend/middleware/sharp-config.js
+++ backend/middleware/sharp-config.js
@@ -1,21 +1,27 @@
-const sharp = require('sharp');
+const sharp = require("sharp");
+const fs = require("fs");
 
-const compressImage = async (req, res, next) => {
-  if (!req.file) {
-    return next();
-  }
+module.exports = async (req, res, next) => {
+    if (!req.file) return next();
 
-  try {
-    const buffer = await sharp(req.file.buffer)
-      .toFormat('webp') // Convertir en format WebP
-      .webp({ quality: 90 }) // Qualité de compression WebP
-      .toBuffer(); // Convertir en tampon
+    try {
+        await sharp(req.file.path)
+            .webp({ quality: 80 })
+            .toFile(`${req.file.path.split(".")[0]}.webp`, (err) => {
+                if (err) {
+                    return res.status(500).json({ error: "Impossible de convertir l'image en format WebP" });
+                }
 
-    req.file.buffer = buffer;
-    next();
-  } catch (error) {
-    next(error);
-  }
+                // Supprimer l'image originale
+                fs.unlink(req.file.path, (error) => {
+                    if (error) {
+                        console.log(error);
+                    }
+                    req.file.path = `${req.file.path.split(".")[0]}.webp`;
+                    next();
+                });
+            });
+    } catch (error) {
+        res.status(500).json({ error: "Impossible d'optimiser l'image" });
+    }
 };
-
-module.exports = compressImage;
