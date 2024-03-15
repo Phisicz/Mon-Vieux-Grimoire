013b4f2933ec42e2557525659e5ee96da4fa3db0
Iuri <nicoguloso2@gmail.com>
2024-03-15 23:35:24 +0100

    fix: refactor a bit the code to be greencode friendly, and fix a few bugs

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -44,6 +44,14 @@ exports.setRating = (req, res, next) => {
 
 // Create a new book in the database
 exports.createBook = (req, res, next) => {
+
+    //Verifies if user has submitted a picture
+    if (!req.file) {
+        return res
+            .status(400)
+            .json({ message: "L'ajout d'une image est obligatoire" });
+    }
+
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
     delete bookObject._userId;
@@ -51,14 +59,18 @@ exports.createBook = (req, res, next) => {
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename.split(".")[0]}.webp`,
         averageRating: 0,
     });
     book.save()
         .then((book) => {
             res.status(201).json({ message: 'Livre enregistré' });
         })
+        //Removes image from images folder in case of an error
         .catch((error) => {
+            fs.unlink(req.file.path, (error) => {
+                req.file.path = `${req.file.path.split(".")[0]}.webp`;
+            });
             res.status(400).json({ error });
         });
 };
@@ -97,30 +109,53 @@ exports.getOneBook = (req, res, next) => {
         });
 };
 
-// Update the book with the provided  _id  with the data provided in the request body.
+// Update the book with the provided _id with the data provided in the request body.
 exports.updateBook = (req, res, next) => {
-    const bookObject = req.file ? {
-        ...JSON.parse(req.body.thing),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
+    const bookObject = req.file
+        ? // Si modification de l'image on copie les nouvelles données
+          {
+              ...JSON.parse(req.body.book),
+              imageUrl: `${req.protocol}://${req.get("host")}/images/${
+                  req.file.filename.split(".")[0]
+              }.webp`,
+          }
+        : { ...req.body };
 
     delete bookObject._userId;
+
     Book.findOne({ _id: req.params.id })
         .then((book) => {
-            if (!book) {
-                return res.status(404).json({ message: "Livre non trouvé" });
-            }
             if (book.userId != req.auth.userId) {
-                return res.status(403).json({ message: 'Non autorisé' });
+                res.status(403).json({ message: "Utilisateur non autorisé" });
+            } else {
+                // Extrait le nom de l'image actuellement stockée
+                const oldPicture = book.imageUrl.split("/").pop();
+
+                Book.updateOne({ _id: req.params.id }, { ...bookObject })
+
+                    .then(() => {
+                        // Pour supprimer l'ancienne image du dossier si une nouvelle image est détectée
+                        if (req.file) {
+                            fs.unlink(`images/${oldPicture}`, (err) => {
+                                if (err) {
+                                    res.status(500).json({ error: err });
+                                } else {
+                                    res.status(200).json({
+                                        message: "Livre modifié",
+                                    });
+                                }
+                            });
+                        } else {
+                            res.status(200).json({ message: "Livre modifié" });
+                        }
+                    })
+                    .catch((error) => res.status(400).json({ error }));
             }
-            Book.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
-                .then(() => res.status(200).json({ message: 'Livre modifié !' }))
-                .catch(error => res.status(404).json({ error }));
         })
-        .catch((error) => {
-            res.status(400).json({ error });
-        });
+        .catch((error) => res.status(500).json({ error }));
 };
+
+
 
 // Delete the single book with the provided _id
 exports.deleteBook = (req, res, next) => {
