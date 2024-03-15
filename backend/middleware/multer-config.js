4925ff85dd495676942359f0768c9ee782bcbf04
Iuri <nicoguloso2@gmail.com>
2024-03-15 23:33:58 +0100

    fix: modify code to be able to include book ID inside file name for better organization

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,32 @@
-const multer = require('multer');
+const multer = require("multer");
 
+// image extensions allowed
 const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
+    "image/jpg": "jpg",
+    "image/jpeg": "jpg",
+    "image/png": "png",
+    "image/webp": "webp",
 };
 
+// Stores uploaded images in the root images folder and renames them
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
+    destination: (req, file, callback) => {
+        callback(null, "images");
+    },
+    filename: (req, file, callback) => {
+        // Replaces spaces with underscores in the original file name
+        const name = file.originalname.split(" ").join("_");
+        const extension = MIME_TYPES[file.mimetype];
+        // Modify the file name to include the WebP extension and the book ID
+        const fileName = req.body.bookId + "_" + Date.now() + "." + extension;
+        callback(null, fileName);
+    },
 });
 
-module.exports = multer({storage: storage, limits: {fileSize: 2 * 1024 * 1024,},}).single("image");
+// file size limit of 2MB
+module.exports = multer({
+    storage: storage,
+    limits: {
+        fileSize: 2 * 1024 * 1024,
+    },
+}).single("image");
