a0ba5ccad2c46c45d541b1d4a1c86eee9d428ec0
Iuri <nicoguloso2@gmail.com>
2024-03-22 02:19:03 +0100

    fix: original images were not being deleted, turned sharp cash to false, and improved code

--- backend/middleware/sharp-config.js
+++ backend/middleware/sharp-config.js
@@ -1,27 +1,34 @@
 const sharp = require("sharp");
-const fs = require("fs");
+const fse = require('fs-extra'); // Utilisez fs-extra pour la suppression
+
+// Désactive la mise en cache de sharp
+sharp.cache(false);
 
 module.exports = async (req, res, next) => {
     if (!req.file) return next();
 
-    try {
-        await sharp(req.file.path)
-            .webp({ quality: 80 })
-            .toFile(`${req.file.path.split(".")[0]}.webp`, (err) => {
-                if (err) {
-                    return res.status(500).json({ error: "Impossible de convertir l'image en format WebP" });
-                }
+    const originalFilePath = req.file.path;
+    const newFilePath = originalFilePath.replace(/\.[^/.]+$/, "") + ".webp";
 
-                // Supprimer l'image originale
-                fs.unlink(req.file.path, (error) => {
-                    if (error) {
-                        console.log(error);
-                    }
-                    req.file.path = `${req.file.path.split(".")[0]}.webp`;
-                    next();
-                });
-            });
+    try {
+        // Convertir l'image en WebP
+        await sharp(originalFilePath)
+            .webp({ quality: 80 })
+            .toFile(newFilePath);
+
+        console.log(`Tentative de suppression du fichier original : ${originalFilePath}`);
+        console.log(`Chemin du nouveau fichier WebP : ${newFilePath}`);
+
+        // Supprimer l'image originale après la conversion
+        await fse.remove(originalFilePath).catch((error) => {
+            console.error(`Erreur lors de la tentative de suppression du fichier original : ${originalFilePath}`, error);
+        });
+
+        // Mise à jour du chemin du fichier dans la requête pour utiliser le nouveau fichier WebP
+        req.file.path = newFilePath;
+        next();
     } catch (error) {
+        console.error("Erreur lors de la conversion ou de la suppression du fichier : ", error);
         res.status(500).json({ error: "Impossible d'optimiser l'image" });
     }
 };
